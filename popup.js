2bc267f52317488db3f32b64f9901e77f823440d
Martin Briand <32023717+martinbriands@users.noreply.github.com>
2019-08-04 14:11:00 -0400

    typos

--- popup.js
+++ popup.js
@@ -39,21 +39,21 @@ button.addEventListener("click", function () {2
 button = document.getElementById("DS");
 button.addEventListener("click", function () {
 	FillPopup("Centivizer Driving Simulator", "BabylonJS - Javascript", "DS",
-	"Driving Simulator targeted towards aging home facilities patients suffering from dementia <br> Developped as a team of two developpers <br> Great number of iterations of the software, <br> with weekly review of new features <br> <br> The game presents an island to drive around, <br> with items randomly generated from a list to collect <br> and famous people and animals you can stop and talk to, the map is generated at loading from a double array <br> <br> The game works with physical modules (wheel, pedals ..) connected to a server that the game get its inputs from <br><br> What I implemented: <br> - Car Controller Input & Logic <br> - Collision Detection <br> - Interaction with characters <br> - Map Generation with customisation <br>",
+	"Driving Simulator targeted towards aging home facilities patients suffering from dementia <br> Developed as a team of two developers <br> Great number of iterations of the software, <br> with weekly review of new features <br> <br> The game presents an island to drive around, <br> with items randomly generated from a list to collect <br> and famous people and animals you can stop and talk to, the map is generated at loading from a double array <br> <br> The game works with physical modules (wheel, pedals ..) connected to a server that the game get its inputs from <br><br> What I implemented: <br> - Car Controller Input & Logic <br> - Collision Detection <br> - Interaction with characters <br> - Map Generation with customisation <br>",
 	"<br><a href=\"centivizer.com\"> centivizer.com </a>")
 });
 
 button = document.getElementById("FishermanFoes");
 button.addEventListener("click", function () {
 	FillPopup("Fisherman Foes", "Unity 2D - C# - TOJAM13", "FishermanFoes",
-	"Unity 2D fighter party game <br> Developped with a team of 4 for Toronto Game Jame 2018 <br><br> Players attack by casting your line and reeling yourself to enemies <br> The goal is to be the highest player for the longest amout of time <br> You can grab onto surfaces to swing and sling yourself <br> The level evolves as the game progresses, changing what you can attach yourself to <br><br> What I implemented: <br> - Hook Mechanic <br> - Character Movement / Animation Rigging <br> - Balistic Trajectory Prediction & Raytracing <br> - Line that folds on contact points with the ground <br> - Controller & Keyboard Support",
+	"Unity 2D fighter party game <br> Developed with a team of 4 for Toronto Game Jame 2018 <br><br> Players attack by casting your line and reeling yourself to enemies <br> The goal is to be the highest player for the longest amout of time <br> You can grab onto surfaces to swing and sling yourself <br> The level evolves as the game progresses, changing what you can attach yourself to <br><br> What I implemented: <br> - Hook Mechanic <br> - Character Movement / Animation Rigging <br> - Balistic Trajectory Prediction & Raytracing <br> - Line that folds on contact points with the ground <br> - Controller & Keyboard Support",
 	"<br><a href=\"https://github.com/jurcaua/TOJAM13\"> Github</a> <br><a href=\"https://jurcaua.itch.io/fisherman-foes\"> Itch.io</a>")
 });
 
 button = document.getElementById("Kleptomanihat");
 button.addEventListener("click", function () {
 	FillPopup("Kleptomanihat", "Unity 2D - C# - GMD 2018", "Kleptomanihat",
-	"UofT Game Development & Design Club's<br> Game Making Deathmatch 2018 <br> 1st place Overall - 1st place Theme <br><br> Unity 2D action platformer <br> Developped with a team of 2 over 2 months <br> Players need to collect hats that stack, each hat having a different effect when you use it <br> <br> What I implemented: <br> - Character Movement and Physics <br> - Hat Interface Class for easy implementation of new Content <br> - Animation Rigging <br> - Area of sight with Raycasting <br>",
+	"UofT Game Development & Design Club's<br> Game Making Deathmatch 2018 <br> 1st place Overall - 1st place Theme <br><br> Unity 2D action platformer <br> Developed with a team of 2 over 2 months <br> Players need to collect hats that stack, each hat having a different effect when you use it <br> <br> What I implemented: <br> - Character Movement and Physics <br> - Hat Interface Class for easy implementation of new Content <br> - Animation Rigging <br> - Area of sight with Raycasting <br>",
 	"<br><a href=\"https://github.com/jurcaua/Kleptomanihat\"> Github</a> <br><a href=\"https://jurcaua.itch.io/kleptomanihat\"> Itch.io</a>")
 });
 
@@ -67,7 +67,7 @@ button.addEventListener("click", function () {
 button = document.getElementById("BreadRampage");
 button.addEventListener("click", function () {
 	FillPopup("Bread Rampage", "Unity 3D VR - C#", "BreadRampage",
-	"Final project for the UofT Game Development <br>& VR Research Program <br><br> Developped with motion sickness in mind, with a slow moving character to reduce nausea <br> You can move around and throw bread to attract nearby birds that attack anything in their path <br> You do not attack yourself, but instead have to use the bird flocking to get rid of the enemies <br> <br> What I implemented: <br> - Bird Flocking Algorithm <br> - Xbox controller & Oculus Rift Support <br> - VR User Interface <br> - Player Movement / Enemy AI <br> - Trajectory Prediction & Line Rendering", 
+	"Final project for the UofT Game Development <br>& VR Research Program <br><br> Developed with motion sickness in mind, with a slow moving character to reduce nausea <br> You can move around and throw bread to attract nearby birds that attack anything in their path <br> You do not attack yourself, but instead have to use the bird flocking to get rid of the enemies <br> <br> What I implemented: <br> - Bird Flocking Algorithm <br> - Xbox controller & Oculus Rift Support <br> - VR User Interface <br> - Player Movement / Enemy AI <br> - Trajectory Prediction & Line Rendering", 
 	"<br><a href=\"https://github.com/martinbriands/BreadRampage\"> Github</a>")
 });
 
@@ -80,8 +80,8 @@ button.addEventListener("click", function () {
 
 button = document.getElementById("GameNani");
 button.addEventListener("click", function () {
-	FillPopup("Game Nani", "Unity Developper tool - C#", "GameNani",
-	"Thacks2 <br> Best Developper Tool Prize <br><br> Developper Tool for Unity Projects that gives a visualisation of player data, aimed at optimizing playtesting <br> Gives relevant data concerning specified objects during playtime <br>(# times looked at, # times interracted with, playtime, time in field of view of Player, Key presses, ...) <br> Easy drag & Drop implementation on objects of interest <br> Data automatically compiled into graphs that can be analyzed with an interface <br><br> What I implemented: <br> - Raycasting Logic <br> - JSON encoding & decoding <br> - Graph Generation <br> - Debugger UI",
+	FillPopup("Game Nani", "Unity Developer tool - C#", "GameNani",
+	"Thacks2 <br> Best Developer Tool Prize <br><br> Developer Tool for Unity Projects that gives a visualisation of player data, aimed at optimizing playtesting <br> Gives relevant data concerning specified objects during playtime <br>(# times looked at, # times interracted with, playtime, time in field of view of Player, Key presses, ...) <br> Easy drag & Drop implementation on objects of interest <br> Data automatically compiled into graphs that can be analyzed with an interface <br><br> What I implemented: <br> - Raycasting Logic <br> - JSON encoding & decoding <br> - Graph Generation <br> - Debugger UI",
 	"<br><a href=\"https://github.com/jurcaua/GameNani\"> Github</a><br><a href=\"https://devpost.com/software/thacks2\"> DevPost</a>")
 });
 
@@ -102,7 +102,7 @@ button.addEventListener("click", function () {
 button = document.getElementById("Pacman");
 button.addEventListener("click", function () {
 	FillPopup("Pacman", "Custom Pacman Engine - C++ & JS", "Pacman",
-	"Pacman C++ Clone originally Developped as a submission for the Ubisoft NXT competition, then revised as a JS Web Game for this site <br> <br>The focus of this game is the ghost AI, each having their own way of attacking the player: <br> - Red chases the player Directly <br> - Pink Chases the player's next intersection <br> - Blue moves randomly <br> - Yellow targets the player then moves randomly when it gets close <br> The other aspect of the game is the customization of the level, that is fully customizable thanks to a double array, allowing to make a classic Pacman Grid or something like the header of this site <br><br> What I implemented: <br> - Player Movement and Input <br> - A* pathfinding algorithm <br> - Custom Map Generation <br> Interface for new types of Ghost",
+	"Pacman C++ Clone originally Developed as a submission for the Ubisoft NXT competition, then revised as a JS Web Game for this site <br> <br>The focus of this game is the ghost AI, each having their own way of attacking the player: <br> - Red chases the player Directly <br> - Pink Chases the player's next intersection <br> - Blue moves randomly <br> - Yellow targets the player then moves randomly when it gets close <br> The other aspect of the game is the customization of the level, that is fully customizable thanks to a double array, allowing to make a classic Pacman Grid or something like the header of this site <br><br> What I implemented: <br> - Player Movement and Input <br> - A* pathfinding algorithm <br> - Custom Map Generation <br> Interface for new types of Ghost",
 	"<br><a href=\"game.html\"> Try the Game</a>")
 });
 
